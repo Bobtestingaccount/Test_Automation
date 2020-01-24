fedfe7273c12f268a8e8503f8ecfb6e717df6b77
gaskinr <pineapplesuprise@gmail.com>
2020-01-24 22:09:55 +0000

    Tests manage Urls now and implementation of a default wait, also added dispose method

--- AutomationTest - Examples/SeleniumTest.cs
+++ AutomationTest - Examples/SeleniumTest.cs
@@ -12,32 +12,31 @@ namespace AutomationTest___Examples
 {
     public class SeleniumTest
     {
-
         public IWebDriver driver;
-
+        public WebDriverWait defaultWait;
+        
         [SetUp]
         public void Setup()
         {
-
-            ////Annoyingly app.config doesn't work will look at later if time but this is typically how I would manage test variables / endpoints etc.
-            //var browser = ConfigurationManager.AppSettings["Browser"];
-
             var browser = "Chrome";
 
             switch (browser)
             {
                 case "Chrome":
-                    driver = new ChromeDriver();
-
-                    //Browser size matters for the website maximize it just for ease of testing
+                    driver = new ChromeDriver(new ChromeOptions { Proxy = null });
                     driver.Manage().Window.Maximize();
                     break;
                 default:
                     throw new NotSupportedException($"Browser {browser} is not supported");
             }
-
-            driver.Url = "http://automationpractice.com/index.php";
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            defaultWait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            driver.Quit();
         }
     }
 }
