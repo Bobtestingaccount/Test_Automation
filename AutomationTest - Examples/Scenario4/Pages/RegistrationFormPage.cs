b195e92cd47f2a2f3df2054b2751aa3cb9a0272c
gaskinr <pineapplesuprise@gmail.com>
2020-01-26 17:46:05 +0000

    Registration form tests

---
+++ AutomationTest - Examples/Scenario4/Pages/RegistrationFormPage.cs
@@ -0,0 +1,81 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationTest___Examples.Scenario4.Pages
+{
+    class RegistrationForm
+    {
+        public IWebDriver driver;
+        public PersonalInformation PersonalInformation;
+        public YourAddress YourAddress;
+
+        public RegistrationForm(IWebDriver driver)
+        {
+            this.driver = driver;
+            PersonalInformation = new PersonalInformation(driver);
+            YourAddress = new YourAddress(driver);
+        }
+        public void Register() => driver.FindElement(By.Id("submitAccount")).Click();
+    }
+
+    public class YourAddress
+    {
+        private IWebDriver driver;
+
+        public YourAddress(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+
+        public void SetAddress(string address) => driver.FindElement(By.Id("address1")).SendKeys(address);
+        public void SetCity(string city) => driver.FindElement(By.Id("city")).SendKeys(city);
+        public void SetState(string state) => driver.FindElement(By.Id("id_state")).SendKeys(state);
+
+        public void SetPostcode(string postcode) => driver.FindElement(By.Id("postcode")).SendKeys(postcode);
+
+        public void SetPhone(string phone) => driver.FindElement(By.Id("phone")).SendKeys(phone);
+    }
+
+    public class PersonalInformation
+    {
+        IWebDriver driver;
+        public PersonalInformation(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+        public void SelectGender(Gender gender)
+        {
+            switch (gender)
+            {
+                case Gender.Male:
+                    driver.FindElement(By.Id("id_gender1")).Click();
+                    break;
+                case Gender.Female:
+                    driver.FindElement(By.Id("id_gender2")).Click();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void SetForename(string forename) => driver.FindElement(By.Id("customer_firstname")).SendKeys(forename);
+
+        public void SetSurname(string forename) => driver.FindElement(By.Id("customer_lastname")).SendKeys(forename);
+
+        public void SetEmail(string forename) => driver.FindElement(By.Id("email")).SendKeys(forename);
+
+        public void SetPassword(string forename) => driver.FindElement(By.Id("passwd")).SendKeys(forename);
+
+
+    }
+}
---
+++ AutomationTest - Examples/Scenario4/RegistrationTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AutomationTest___Examples.Scenario4.Pages;
+using OpenQA.Selenium;
+using Shouldly;
+
+namespace AutomationTest___Examples
+{
+    class RegistrationTest
+    {
+        [TestFixture]
+        public class InvalidFormSubmission : SeleniumTest
+        {
+            public void EmptyForm()
+            {
+                driver.Url = "http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation";
+                //Random Email
+                var randomEmail = $"{Guid.NewGuid().ToString().Substring(0, 5)}@test.com";
+                driver.FindElement(By.Id("email_create")).SendKeys(randomEmail);
+                driver.FindElement(By.Id("SubmitCreate")).Click();
+
+                RegistrationForm form = new RegistrationForm(driver);
+
+                form.Register();
+
+                //I'd actually write a better test for this, but I actually can't not register now!
+                driver.FindElement(By.Id("center_column")).Text.ShouldNotContain("MY ACCOUNT");
+            }
+        }
+
+        [TestFixture]
+        public class ValidFormSubmission : SeleniumTest
+        {
+            [Test]
+            public void ValidRegistration()
+            {
+                driver.Url = "http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation";
+                //Random Email
+                var randomEmail = $"{Guid.NewGuid().ToString().Substring(0, 5)}@test.com";
+                driver.FindElement(By.Id("email_create")).SendKeys(randomEmail);
+                driver.FindElement(By.Id("SubmitCreate")).Click();
+
+                RegistrationForm form = new RegistrationForm(driver);
+
+                form.PersonalInformation.SetForename("Test");
+                form.PersonalInformation.SetSurname("McTest");
+                form.PersonalInformation.SetPassword("password");
+
+                form.YourAddress.SetAddress("123 Test Street");
+                form.YourAddress.SetCity("Test City");
+                form.YourAddress.SetState("Alabama");
+                form.YourAddress.SetPostcode("99524");
+                form.YourAddress.SetPhone("0113123123");
+
+                form.Register();
+
+                driver.FindElement(By.Id("center_column")).Text.ShouldContain("MY ACCOUNT");
+            }
+        }
+    }
+}
