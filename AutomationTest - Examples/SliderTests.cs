3233c8f17eedadd4ce124986c198b514273bfbb0
gaskinr <pineapplesuprise@gmail.com>
2020-01-26 16:02:35 +0000

    Finish slider test

--- AutomationTest - Examples/SliderTests.cs
+++ AutomationTest - Examples/SliderTests.cs
@@ -1,10 +1,12 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AutomationTest___Examples
 {
@@ -23,10 +25,31 @@ namespace AutomationTest___Examples
 
             private void ProductsInBandAreSelected(int min, int max)
             {
-                defaultWait.Until(c => c.FindElements(By.TagName("br"))
-                .Single(c => c.Text.Contains("Loading")).Displayed == false);
-                driver.FindElements(By.Id("product-container"));
+                //Disabled due to a bug where the loading button will never dissapear
+                //And the filterning never happens
+
+                //var loading = driver.FindElements(By.TagName("p"));
+                //defaultWait.Until(c => c.FindElements(By.TagName("p"))?
+                //.Single(c => c.Text.Contains("Loading")).Displayed == false);
+
+                var products = driver.FindElements(By.ClassName("product-container"));
+                var prices = new List<int>();
+
+                foreach (var product in products)
+                {
+                    var priceOfProduct = product.FindElement(By.ClassName("price"));
+                    var individualPrice = GetPriceAsInt(priceOfProduct.GetAttribute("innerText"));
+                    prices.Add(individualPrice);
+                }
+
+                foreach(var price in prices)
+                {
+                    price.ShouldBeGreaterThan(min);
+                    price.ShouldBeLessThan(max);
+                }
             }
+
+            private int GetPriceAsInt(string text) => Convert.ToInt32(Regex.Match(text, @"(?<=\$)\d+?(?=\.)").Value);
 
             private void LaunchProductPage()
             {
@@ -35,6 +58,7 @@ namespace AutomationTest___Examples
 
             private void SetPriceSlider(int min, int max)
             {
+                //Change these if the value on the slider changes.
                 int minValueOnSlider = 16;
                 int maxValueOnSlider = 32;
 
@@ -48,16 +72,13 @@ namespace AutomationTest___Examples
                 double percentMin = Math.Abs((double)(min - rangeOnSlider) / rangeOnSlider);
                 double percentMax = (1 - (Math.Abs((double)(rangeOnSlider - max) / rangeOnSlider)));
 
-
+                //Calculate the distance to move the slider based on it's size.
                 var slider = driver.FindElement(By.Id("layered_price_slider"));
                 var control = slider.FindElements(By.TagName("a"));
-
                 var minSliderButton = control[0];
                 var maxSliderButton = control[1];
-
                 var minMoveBy = Math.Round(slider.Size.Width * percentMin);
                 var maxMoveBy = -Math.Round((slider.Size.Width * percentMax));
-
 
                 Actions minSliderAction = new Actions(driver);
                 Actions maxSliderAction = new Actions(driver);
